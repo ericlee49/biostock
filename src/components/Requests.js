6bc6d14988fecb589dd37ef5d9475efc596bd2b0
Eric Lee <ericlee49@gmail.com>
2019-08-23 15:15:59 -0700

    using hooks for form

--- src/components/Requests.js
+++ src/components/Requests.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container'
@@ -11,6 +11,12 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogActions from '@material-ui/core/DialogActions';
 import Box from '@material-ui/core/Box';
+
+// Forms:
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import OutlinedInput from '@material-ui/core/OutlinedInput'
+
 
 import gql from 'graphql-tag';
 import {Mutation} from 'react-apollo';
@@ -62,16 +68,40 @@ function ConfirmationDialog(props) {
     )
 }
 
+//custom hook
+function useFormInput (initialValue) {
+    const [value, setValue] = useState(initialValue);
+    const handleChange = event => {
+        setValue(event.target.value);
+        console.log(value);
+    }
+    return {
+        value,
+        onChange: handleChange,
+    };
+};
+
 export default function Requests() {
     const [confimrationDialogOpen, setConfirmationDialogOpen] = React.useState(false);
+    const testInput = useFormInput('');
+
+    //Inputs using custom useFormInput hook
+    const firstname = useFormInput('');
+    const lastname = useFormInput('');
+    const email = useFormInput('');
+    const description = useFormInput('');
 
     function handleClickOpen() {
         setConfirmationDialogOpen(true);
+        console.log(testInput.value);
     }
 
     function handleClose() {
         setConfirmationDialogOpen(false);
-    }    
+    }
+    
+
+
     return (
         <Box pt={10}>
         <Container component="main" maxWidth="sm">
@@ -88,6 +118,7 @@ export default function Requests() {
                             margin="normal"
                             fullWidth
                             label="First Name"
+                            {...firstname}
                         />                    
                     </Grid>
                     <Grid item xs={12} sm={6}>
@@ -97,6 +128,7 @@ export default function Requests() {
                             margin="normal"
                             fullWidth
                             label="Last Name"
+                            {...lastname}
                         />                    
                     </Grid>
                     <Grid item sm={12}>
@@ -107,6 +139,7 @@ export default function Requests() {
                             required
                             fullWidth
                             label="Email"
+                            {...email}
                         />                    
                     </Grid>
                     <Grid item sm={12}>
@@ -118,8 +151,21 @@ export default function Requests() {
                             required
                             fullWidth
                             label="Description"
+                            {...description}
                         /> 
                         <p>Please Email <a href="mailto:craig.kornak@ubc.ca?Subject=BioStock%20Request" target="_top">Craig</a> if you have any images you wish to send</p>                   
+                    </Grid>
+
+                    <Grid item sm={12}>
+                        <TextField
+                            variant="outlined"
+                            name="test"
+                            margin="normal"
+                            required
+                            fullWidth
+                            label="Test"
+                            {...testInput}
+                        />                
                     </Grid>
                     <Button
                         // type="submit"
