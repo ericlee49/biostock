9abee4d14faf53caaedc673689ab05aae73ea661
Eric Lee <ericlee49@gmail.com>
2019-08-27 16:24:43 -0700

    updates on request page

--- src/components/Requests.js
+++ src/components/Requests.js
@@ -12,18 +12,17 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogActions from '@material-ui/core/DialogActions';
 import Box from '@material-ui/core/Box';
 
-// Forms:
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
-import OutlinedInput from '@material-ui/core/OutlinedInput'
-
-
 import gql from 'graphql-tag';
 import {Mutation} from 'react-apollo';
 
+import 'typeface-signika';
+
+import {Redirect} from 'react-router-dom';
+
+
 const CREATE_PHOTO_REQUEST = gql `
     mutation CreatePhotoRequest($firstname: String!, $lastname: String!, $email: String!, $description: String!) {
-        createPhotoRequest(input: {
+        createPhotorequest(input: {
             data: {
                 firstname: $firstname,
                 lastname: $lastname,
@@ -47,11 +46,23 @@ const CREATE_PHOTO_REQUEST = gql `
 
 function ConfirmationDialog(props) {
     const {open, onClose} = props;
+
+    // const handleDialogClose = () => {
+    //     // onClose();
+    //     return (
+    //         <Redirect to="/" />
+    //     );
+    // }
+
+    function handleDialogClose() {
+        onClose();
+    }
+
     return (
         <Dialog
             open={open}
             maxWidth="sm"
-            onClose={onClose}
+            onClose={handleDialogClose}
         >
             <DialogTitle id="confirmation-dialog-title">{"Request Received!"}</DialogTitle>
             <DialogContent>
@@ -60,7 +71,7 @@ function ConfirmationDialog(props) {
                 </DialogContentText>                
             </DialogContent>
             <DialogActions>
-                <Button color="primary" onClick={onClose}>
+                <Button color="primary" onClick={() => {return <Redirect to="/" />}}>
                     OK
                 </Button>
             </DialogActions>
@@ -91,9 +102,9 @@ export default function Requests() {
     const email = useFormInput('');
     const description = useFormInput('');
 
+
     function handleClickOpen() {
         setConfirmationDialogOpen(true);
-        console.log(testInput.value);
     }
 
     function handleClose() {
@@ -106,79 +117,88 @@ export default function Requests() {
         <Box pt={10}>
         <Container component="main" maxWidth="sm">
             <ConfirmationDialog open={confimrationDialogOpen} onClose={handleClose}/>
-            <Typography component="h1" variant="h5">
+            <Typography variant="h4">
+                <Box fontFamily="Signika">
                 Send us a Request!
+                </Box>
             </Typography>
-            <form noValidate>
-                <Grid container spacing={2}>
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            variant="outlined"
-                            name="firstname"
-                            margin="normal"
-                            fullWidth
-                            label="First Name"
-                            {...firstname}
-                        />                    
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            variant="outlined"
-                            name="lastname"
-                            margin="normal"
-                            fullWidth
-                            label="Last Name"
-                            {...lastname}
-                        />                    
-                    </Grid>
-                    <Grid item sm={12}>
-                        <TextField
-                            variant="outlined"
-                            name="email"
-                            margin="normal"
-                            required
-                            fullWidth
-                            label="Email"
-                            {...email}
-                        />                    
-                    </Grid>
-                    <Grid item sm={12}>
-                        <TextField
-                            variant="outlined"
-                            multiline
-                            margin="normal"
-                            rows="4"
-                            required
-                            fullWidth
-                            label="Description"
-                            {...description}
-                        /> 
-                        <p>Please Email <a href="mailto:craig.kornak@ubc.ca?Subject=BioStock%20Request" target="_top">Craig</a> if you have any images you wish to send</p>                   
-                    </Grid>
-
-                    <Grid item sm={12}>
-                        <TextField
-                            variant="outlined"
-                            name="test"
-                            margin="normal"
-                            required
-                            fullWidth
-                            label="Test"
-                            {...testInput}
-                        />                
-                    </Grid>
-                    <Button
-                        // type="submit"
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        variant="outlined"
+                        name="firstname"
+                        margin="normal"
                         fullWidth
-                        variant="contained"
-                        color="primary"
-                        onClick={handleClickOpen}
-                    >
-                        Submit
-                    </Button>
+                        label="First Name"
+                        {...firstname}
+                    />                    
                 </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        variant="outlined"
+                        name="lastname"
+                        margin="normal"
+                        fullWidth
+                        label="Last Name"
+                        {...lastname}
+                    />                    
+                </Grid>
+                <Grid item sm={12}>
+                    <TextField
+                        variant="outlined"
+                        name="email"
+                        margin="normal"
+                        required
+                        fullWidth
+                        label="Email"
+                        {...email}
+                    />                    
+                </Grid>
+                <Grid item sm={12}>
+                    <TextField
+                        variant="outlined"
+                        multiline
+                        margin="normal"
+                        rows="4"
+                        required
+                        fullWidth
+                        label="Description"
+                        {...description}
+                    /> 
+                    <Typography variant="p">
+                        <Box fontFamily="Signika">
+                        Please Email <a href="mailto:craig.kornak@ubc.ca?Subject=BioStock%20Request" target="_top">Craig</a> if you have any images you wish to send
+                        </Box>
+                    </Typography>
+                </Grid>
+                <Mutation
+                    mutation={CREATE_PHOTO_REQUEST}
+                    variables={{
+                        firstname: firstname.value,
+                        lastname: lastname.value,
+                        email: email.value,
+                        description: description.value
+                    }}
+                    onCompleted={handleClickOpen}
+                >
+                    {(createPhotorequest) => (
+                        <Grid item sm={12}>
+                            <Button
+                                fullWidth
+                                variant="contained"
+                                color="primary" 
+                                onClick={createPhotorequest}                       
+                            >
+                                <Box fontFamily="Signika">
+                                    Submit Request
+                                </Box>
+                            </Button>
+                        </Grid>
+                    )}
 
-            </form>
+                </Mutation>
+            </Grid>
+
         </Container>
         </Box>
     )
